9e02b0835a48d9f5f80a1b58c36d608e44b7eb78
Oliver Rauch <Oliver.Rauch@xsane.org>
2007-02-25 20:22:00 +0100

    import xsane-0.993.tar.gz

--- include/config.h
+++ include/config.h
@@ -99,10 +99,10 @@
 #define PACKAGE "xsane"
 
 /* Define to the version of the distribution.  */
-#define VERSION "0.992"
+#define VERSION "0.993"
 
 /* The concatenation of the strings PACKAGE, "-", and VERSION.  */
-#define XSANE_PACKAGE_VERSION "xsane-0.992"
+#define XSANE_PACKAGE_VERSION "xsane-0.993"
 
 /* Define if you have the __argz_count function.  */
 #define HAVE___ARGZ_COUNT 1
